afbc1dfeb82ab9d71871b9646332b20636079fd9
Alex W <alex.seungmin.woo@gmail.com>
2023-10-05 03:16:11 -0400

    Added basic skeleton constructor of ReturnPlay

--- Chess.java
+++ Chess.java
@@ -37,9 +37,14 @@ class ReturnPlay {
 				  RESIGN_BLACK_WINS, RESIGN_WHITE_WINS, 
 				  CHECK, CHECKMATE_BLACK_WINS,	CHECKMATE_WHITE_WINS, 
 				  STALEMATE};
-	
+
 	ArrayList<ReturnPiece> piecesOnBoard;
 	Message message;
+
+	public ReturnPlay (ArrayList<ReturnPiece> piecesOnBoard)
+	{
+		
+	}
 }
 
 public class Chess {
