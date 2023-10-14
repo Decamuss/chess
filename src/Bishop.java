91b8ed48996dd9ee9f69bf0e7be772f23504fdff
Rick Hsu <rick.hsu2010@gmail.com>
2023-10-14 19:28:32 -0400

    changes to piece and bishop

--- src/Bishop.java
+++ src/Bishop.java
@@ -2,30 +2,45 @@ package chess;
 
 
 
-public class Bishop extends Piece {
-    
-    // Constructor to set the color of the Bishop
-    public Bishop(boolean white) {
-        super(white);  // This calls the constructor of the Piece class
-    }
-    
-    @Override
-    public boolean canMove(int oldX, int oldY, int newX, int newY, boolean isNewSpotEmpty) {
-        // Bishops move diagonally, so the absolute difference between the x-coordinates
-        // and y-coordinates of the old position and the new position should be equal.
-        return Math.abs(oldX - newX) == Math.abs(oldY - newY);
-    }
-    
-    @Override
-    public void movePiece() {
-        // Implement the logic to move the piece.
-        // This might involve updating the board and the piece's position.
-    }
-    
-    @Override
-    public String drawPiece() {
-        // Return a string representation of the Bishop.
-        // For simplicity, we'll use "B" to represent a Bishop.
-        return isWhite() ? "B" : "b";
-    }
+public class Bishop extends Piece{
+
+	private String name = "B";
+	
+	public Bishop(boolean w){
+		this.setWhite(w);
+		
+		if (w == true){
+			this.name = "w" + this.name; 
+		}else{
+			this.name = "b" + this.name; 
+		}
+		
+	}
+	
+	public boolean canMove(int oldX, int oldY, int newX, int newY, boolean isNewSpotEmpty) {
+
+		int changeX;
+		int changeY;
+		
+		changeX = Math.abs(newX-oldX);
+		changeY = Math.abs(newY-oldY);
+		
+		if (deltaX == deltaY) {
+			//System.out.println("canMove:true");
+			return true;
+		}
+		
+		//System.out.println("canMove:false");
+		
+		return false;
+	}
+
+	public String drawPiece() {
+		return this.name;
+	}
+
+	public void movePiece() {
+		
+	}
+
 }
--- src/Piece.java
+++ src/Piece.java
@@ -1,68 +1,22 @@
-package chess;
-
-import java.util.List;
-
-// Abstract class representing a general chess piece
 public abstract class Piece {
-    
-    // Enum for Piece Color
-    public enum Color {
-        WHITE, BLACK;
-    }
 
-    // Member variables
-    protected Color color;
-    protected int x, y;
+	private boolean white = true;
+	
+	public boolean firstMove = true;
+	
+	public void setWhite(boolean t){
+		white = t;
+	}
+	
 
-    // Constructor
-    public Piece(Color color, int x, int y) {
-        this.color = color;
-        this.x = x;
-        this.y = y;
-    }
-
-    private boolean white = true;
-    public boolean firstMove = true;
-
-    // New constructor
-    public Piece(boolean white) {
-        this.white = white;
-    }
-
-    // Abstract method to be implemented by each specific type of piece
-    public abstract List<String> getPossibleMoves();
-
-    // Getters for color and position
-    public Color getColor() {
-        return color;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    // Method to move the piece to a new position
-    public void moveTo(int newX, int newY) {
-        this.x = newX;
-        this.y = newY;
-    }
+	public boolean isWhite(){
+		return this.white;
+	}
+	
+	
+	public abstract boolean canMove(int oldX, int oldY, int newX, int newY, boolean isNewSpotEmpty);
+	
+	public abstract void movePiece();
+	
+	public abstract String drawPiece();
 }
-
-// Subclass for Pawn
-class Pawn extends Piece {
-    public Pawn(Color color, int x, int y) {
-        super(color, x, y);
-    }
-
-    @Override
-    public List<String> getPossibleMoves() {
-        // Implement pawn-specific logic to generate possible moves
-        return null;  // Placeholder
-    }
-}
-
-// Similarly, you would create subclasses for other types of pieces: Rook, Knight, Bishop, Queen, and King.
