d2b1e6ddaa86add3a689b5d306bd8de40ba56bed
Rick Hsu <rick.hsu2010@gmail.com>
2023-10-14 20:04:32 -0400

    Remove Piece.Java Change Bishop.java

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,46 +1,49 @@
 package chess;
 
+public class Bishop extends ReturnPiece {
 
+    public Bishop(PieceType pieceType, PieceFile pieceFile, int pieceRank) {
+        this.pieceType = pieceType;  // Assume PieceType.BB for black bishop or PieceType.WB for white bishop
+        this.pieceFile = pieceFile;  // Assume PieceFile.a through PieceFile.h
+        this.pieceRank = pieceRank;  // Assume 1 through 8
+    }
 
-public class Bishop extends Piece{
+    public boolean isLegalMove(int oldX, int oldY, int newX, int newY, Board board) {
+        // Check if the move is along a diagonal
+        if (Math.abs(oldX - newX) != Math.abs(oldY - newY)) {
+            return false;
+        }
 
-	private String name = "B";
-	
-	public Bishop(boolean w){
-		this.setWhite(w);
-		
-		if (w == true){
-			this.name = "w" + this.name; 
-		}else{
-			this.name = "b" + this.name; 
-		}
-		
-	}
-	
-	public boolean canMove(int oldX, int oldY, int newX, int newY, boolean isNewSpotEmpty) {
+        // Determine the direction of the move
+        int xDirection = (newX > oldX) ? 1 : -1;
+        int yDirection = (newY > oldY) ? 1 : -1;
 
-		int changeX;
-		int changeY;
-		
-		changeX = Math.abs(newX-oldX);
-		changeY = Math.abs(newY-oldY);
-		
-		if (deltaX == deltaY) {
-			//System.out.println("canMove:true");
-			return true;
-		}
-		
-		//System.out.println("canMove:false");
-		
-		return false;
-	}
+        // Check for any pieces blocking the path
+        int x = oldX + xDirection;
+        int y = oldY + yDirection;
+        while (x != newX && y != newY) {
+            if (!board.isSpotEmpty(x, y) || board.isSameColor(x, y, this.isWhite())) {
+                return false;
+            }
+            x += xDirection;
+            y += yDirection;
+        }
 
-	public String drawPiece() {
-		return this.name;
-	}
+        // Check if the destination spot is empty or contains an opponent's piece
+        if (!board.isSpotEmpty(newX, newY) && board.isSameColor(newX, newY, this.isWhite())) {
+            return false;
+        }
 
-	public void movePiece() {
-		
-	}
+        return true;
+    }
 
+    public void move(int newX, int newY) {
+        this.pieceFile = PieceFile.values()[newX];
+        this.pieceRank = newY + 1;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ":Bishop";
+    }
 }
