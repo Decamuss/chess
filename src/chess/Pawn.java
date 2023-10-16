651e8fddada3146715e91b59b71aaa5252ae4eff
Rick Hsu <rick.hsu2010@gmail.com>
2023-10-16 14:22:57 -0400

    Pawn check fix

--- src/chess/Pawn.java
+++ src/chess/Pawn.java
@@ -16,10 +16,15 @@ public class Pawn extends ReturnPiece implements Piece{
     }
 
     public boolean isLegalMove(int oldX, int oldY, int newX, int newY, ArrayList<ReturnPiece> piecesOnBoard) {
+        // First, check basic move legality
         if (!isLegalMoveWithoutCheck(oldX, oldY, newX, newY, piecesOnBoard)) {
             return false;  // Move is not legal
         }
-
+    
+        oldY -= 1;
+        newY -= 1;
+        int direction = (this.pieceType.toString().charAt(0) == 'W') ? 1 : -1;  // White pawns move up, black pawns move down
+    
         // Save the current state
         PieceFile originalFile = this.pieceFile;
         int originalRank = this.pieceRank;
@@ -29,40 +34,35 @@ public class Pawn extends ReturnPiece implements Piece{
         this.pieceFile = PieceFile.values()[newX];
         this.pieceRank = newY + 1;
         this.hasMoved = true;
-
+    
         char kingColor = this.pieceType.toString().charAt(0);
         boolean kingInCheck = board.isKingInCheck(piecesOnBoard, kingColor);
-
+    
         // Undo the simulated move
         this.pieceFile = originalFile;
         this.pieceRank = originalRank;
         this.hasMoved = originalHasMoved;
-
+    
         // Now check if the king is in check
         if (kingInCheck) {
             return false;  // Move is illegal, it puts the king in check
         }
-
-        oldY -=1;
-        newY -=1;
-        int direction = (this.pieceType.toString().charAt(0) == 'W') ? 1 : -1;  // White pawns move up, black pawns move down
+    
         // Check for initial two-step forward move
         if (hasMoved == false && oldX == newX && oldY + (2 * direction) == newY && isSpotEmpty(newX, newY, piecesOnBoard) && isSpotEmpty(newX, oldY + direction, piecesOnBoard)) {
             this.hasMoved = true;
-            return true;
-        }
-
-        // Proceed with the capturing logic only if the king is not in check
-        if ((oldX + 1 == newX || oldX - 1 == newX) && oldY + direction == newY && !isSpotEmpty(newX, newY, piecesOnBoard) && !isSameColor(oldX, oldY, newX, newY, piecesOnBoard)) {
+        } else if ((oldX + 1 == newX || oldX - 1 == newX) && oldY + direction == newY && !isSpotEmpty(newX, newY, piecesOnBoard) && !isSameColor(oldX, oldY, newX, newY, piecesOnBoard)) {
             // Capture opponent's piece
             ReturnPiece capturedPiece = getPieceAt(newX, newY, piecesOnBoard);
             piecesOnBoard.remove(capturedPiece);
-            return true;
+        } else {
+            // If none of the above, then it's just a standard move.
+            this.hasMoved = true;
         }
-        
-        this.hasMoved = true;
+    
         return true;  // Move is legal
     }
+    
 
     public boolean isLegalMoveWithoutCheck(int oldX, int oldY, int newX, int newY, ArrayList<ReturnPiece> piecesOnBoard) {
         oldY -=1;
